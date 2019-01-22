b393a33488c425abcd92861a45aab6eab690dfd7
Mitch Greene <m.iangreene@gmail.com>
2019-01-21 23:54:23 -0500

    Day 103 of 100DoCC

--- app/src/main/java/codemaestro/co/chronometerwithviewmodel/MainActivity.java
+++ app/src/main/java/codemaestro/co/chronometerwithviewmodel/MainActivity.java
@@ -5,10 +5,12 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
     ChronometerViewModel chronometerViewModel;
+    Chronometer chronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +19,32 @@ public class MainActivity extends AppCompatActivity {
 
         chronometerViewModel = ViewModelProviders.of(this).get(ChronometerViewModel.class);
 
-        Chronometer chronometer = findViewById(R.id.chronometer);
+        chronometer = findViewById(R.id.chronometer);
 
         if (chronometerViewModel.getStartTime() == null) {
             // If the start date is not defined, it's a new ViewModel so set it.
             long startTime = SystemClock.elapsedRealtime();
             chronometerViewModel.setStartTime(startTime);
-            chronometer.setBase(startTime);
+//            chronometer.setBase(startTime);
         } else {
             // Otherwise the ViewModel has been retained, set the chronometer's base to the original
             // starting time.
-            chronometer.setBase(chronometerViewModel.getStartTime());
+//            chronometer.setBase(chronometerViewModel.getStartTime());
         }
 
+
+    }
+
+    public void startButton(View view) {
         chronometer.start();
+    }
+
+    public void pauseButton(View view) {
+    }
+
+    public void resetButton(View view) {
+    }
+
+    public void commitButton(View view) {
     }
 }
